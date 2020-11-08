eb6a35eb6dbbb862015e5b55f41d36ca66f46ca5
chi-hao.lay@outlook.com <development@cschlay.com>
2020-11-08 10:15:54 +0200

    Add basic and complex test to at.test.js

--- test/at.test.js
+++ test/at.test.js
@@ -2,5 +2,41 @@ import at from "../src/at.js";
 import chai from "chai";
 
 describe("at.js", () => {
-  it("single path", () => {});
+  it("simple path", () => {
+    chai.expect(at({"a": 1}, ["a"])).to.deep.equal([1]);
+  });
+  describe("complex object 'house'", () => {
+    const obj = {
+      house: {
+        toilets: [
+          [
+            "sink",
+            "squatToilet"
+          ],
+          [
+            "bidet",
+            "ordinaryToilet"
+          ]
+        ],
+        kitchen: {
+          fridge: ["cake", "iceCream"]
+        }
+      }
+    };
+    it("the first items in 'fridge'", () => {
+      chai.expect(at(obj, "house.kitchen.fridge[0]", "house.kitchen.fridge[1]")).to.deep.equal(["cake", "iceCream"]);
+    })
+    it("squatToilet in first toilet", () => {
+      chai.expect(at(obj, "house.toilets[0][1]")).to.deep.equal(["squatToilet"]);
+    })
+    it("the contents of house", () => {
+      chai.expect(at(obj, "house.toilets", "house.kitchen.fridge"), [
+        [
+          ["sink","squatToilet"],
+          ["bidet","ordinaryToilet"],
+          ["cake", "iceCream"]
+        ]
+      ])
+    })
+  });
 });
