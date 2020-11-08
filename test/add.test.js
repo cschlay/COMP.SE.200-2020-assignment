7c7ef63e32fe9ac8f9f15160e1db5bdb334bdc6b
chi-hao.lay@outlook.com <development@cschlay.com>
2020-11-08 10:25:48 +0200

    Add eslintrc to enforce the code style to meet the style things used in src

--- test/add.test.js
+++ test/add.test.js
@@ -1,41 +1,41 @@
-import add from "../src/add.js";
-import chai from "chai";
+import add from '../src/add.js'
+import chai from 'chai'
 
-describe("add.js", () => {
-  it("adds positive numbers", () => {
-    chai.expect(add(9.5, 100)).to.equal(109.5);
-  });
+describe('add.js', () => {
+  it('adds positive numbers', () => {
+    chai.expect(add(9.5, 100)).to.equal(109.5)
+  })
 
-  it("adds negative numbers", () => {
-    chai.expect(add(-999, -1)).to.equal(-1000);
-  });
+  it('adds negative numbers', () => {
+    chai.expect(add(-999, -1)).to.equal(-1000)
+  })
 
-  it("adds a negative and a positive number", () => {
-    chai.expect(add(-900, 100)).to.equal(-800);
-    chai.expect(add(900, -100)).to.equal(800);
-  });
+  it('adds a negative and a positive number', () => {
+    chai.expect(add(-900, 100)).to.equal(-800)
+    chai.expect(add(900, -100)).to.equal(800)
+  })
 
-  describe("augend is not a number", () => {
-    const augend = Math.floor(Math.random() * 10000 - Math.random() * 10000);
-    it("addend is empty string", () => {
-      chai.expect(add(augend, "")).to.equal(augend.toString());
-    });
-    it("addend is null or undefined", () => {
-      chai.expect(add(augend, null)).to.equal(augend);
-      chai.expect(add(augend, undefined)).to.equal(augend);
-    });
-    it("addednd is a object", () => {
-      chai.expect(add(augend, { json: "" })).to.be.NaN;
-    });
-    it("addend is a function", () => {
-      chai.expect(add(augend, () => {})).to.be.NaN;
-    });
-    it("addend is a string", () => {
+  describe('augend is not a number', () => {
+    const augend = Math.floor(Math.random() * 10000 - Math.random() * 10000)
+    it('addend is empty string', () => {
+      chai.expect(add(augend, '')).to.equal(augend.toString())
+    })
+    it('addend is null or undefined', () => {
+      chai.expect(add(augend, null)).to.equal(augend)
+      chai.expect(add(augend, undefined)).to.equal(augend)
+    })
+    it('addednd is a object', () => {
+      chai.expect(add(augend, { json: '' })).to.be.NaN
+    })
+    it('addend is a function', () => {
+      chai.expect(add(augend, () => {})).to.be.NaN
+    })
+    it('addend is a string', () => {
       // This case concatenates, i think it shouldn't do it.
-      chai.expect(add(augend, "str")).to.be.NaN;
-    });
-    it("addend is an array", () => {
-      chai.expect(add(augend, [3])).to.be.NaN;
-    });
-  });
-});
+      chai.expect(add(augend, 'str')).to.be.NaN
+    })
+    it('addend is an array', () => {
+      chai.expect(add(augend, [3])).to.be.NaN
+    })
+  })
+})
--- test/at.test.js
+++ test/at.test.js
@@ -1,42 +1,50 @@
-import at from "../src/at.js";
-import chai from "chai";
+import at from '../src/at.js'
+import chai from 'chai'
 
-describe("at.js", () => {
-  it("simple path", () => {
-    chai.expect(at({"a": 1}, ["a"])).to.deep.equal([1]);
-  });
-  describe("complex object 'house'", () => {
+describe('at.js', () => {
+  it('simple path', () => {
+    chai.expect(at({'a': 1}, ['a'])).to.deep.equal([1])
+  })
+
+  describe('complex object "house"', () => {
     const obj = {
       house: {
         toilets: [
           [
-            "sink",
-            "squatToilet"
+            'sink',
+            'squatToilet'
           ],
           [
-            "bidet",
-            "ordinaryToilet"
+            'bidet',
+            'ordinaryToilet'
           ]
         ],
         kitchen: {
-          fridge: ["cake", "iceCream"]
+          fridge: ['cake', 'iceCream']
         }
       }
-    };
-    it("the first items in 'fridge'", () => {
-      chai.expect(at(obj, "house.kitchen.fridge[0]", "house.kitchen.fridge[1]")).to.deep.equal(["cake", "iceCream"]);
+    }
+
+    it('the first items in "fridge"', () => {
+      chai.expect(at(obj, 'house.kitchen.fridge[0]', 'house.kitchen.fridge[1]')).to.deep.equal(['cake', 'iceCream'])
     })
-    it("squatToilet in first toilet", () => {
-      chai.expect(at(obj, "house.toilets[0][1]")).to.deep.equal(["squatToilet"]);
+
+    it('squatToilet in first toilet', () => {
+      chai.expect(at(obj, 'house.toilets[0][1]')).to.deep.equal(['squatToilet'])
     })
-    it("the contents of house", () => {
-      chai.expect(at(obj, "house.toilets", "house.kitchen.fridge"), [
+
+    it('the contents of house', () => {
+      chai.expect(at(obj, 'house.toilets', 'house.kitchen.fridge'), [
         [
-          ["sink","squatToilet"],
-          ["bidet","ordinaryToilet"],
-          ["cake", "iceCream"]
+          ['sink','squatToilet'],
+          ['bidet','ordinaryToilet'],
+          ['cake', 'iceCream']
         ]
       ])
     })
-  });
-});
+  })
+
+  it('object is empty', () => {
+    chai.expect(at({}, 'wardrobe.jacket')).to.deep.equal([undefined])
+  })
+})
