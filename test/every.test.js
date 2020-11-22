ff681eb500645146a7f485cef4a067235130cb78
chi-hao.lay@outlook.com <development@cschlay.com>
2020-11-22 13:47:49 +0200

    Implement every

---
+++ test/every.test.js
@@ -0,0 +1,26 @@
+import every from '../src/every.js'
+import chai, { AssertionError } from 'chai'
+
+describe('every.js', () => {
+    it('using simple function that results false', () => {
+        chai.expect(
+            every(['euro', 'dollar'], (item) => item !== 'dollar')
+        ).to.be.false
+    })
+
+    it('using simple function that results true', () => {
+        chai.expect(
+            every(['euro'], (item) => item === 'euro')
+        ).to.be.true
+    })
+
+    it('array is not array should throw error', () => {
+        chai.expect(() => every(1232, () => {
+            return false
+        })).to.throw(TypeError)
+    })
+
+    it('predicate not being function should throw error', () => {
+        chai.expect(() => every([23], 'not func')).to.throw(TypeError)
+    })
+})
