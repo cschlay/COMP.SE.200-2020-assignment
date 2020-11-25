6c23e375ffa448c0a109a0599d32d6c51059ddb4
linnat <linnat@student.tut.fi>
2020-11-25 17:30:03 +0200

    fixed array comparing

--- test/keys.test.js
+++ test/keys.test.js
@@ -7,13 +7,13 @@ describe('Keys.js',()=>{
             this.a = 1
             this.b = 2
         }
-        chai.expect(Keys(test_function)).to.equal(['a','b'])
+        chai.expect(Keys(test_function)).to.deep.equal(['a','b'])
     })
     it('testing different values',()=>{
 
         var nice_array = ['Watz','nakki','cat']
         
-        chai.expect(Keys(nice_array)).to.equal(['0','1','2'])
-        chai.expect(Keys(nice_array[1])).to.equal(['0','1'])
+        chai.expect(Keys(nice_array)).to.deep.equal(['0','1','2'])
+        chai.expect(Keys(nice_array[1])).to.deep.equal(['0','1','2','3','4'])
     })
 })
